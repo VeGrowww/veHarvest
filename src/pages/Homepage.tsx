f560b411733e14d5b9ea20cd921edc430af81040
Alex Dong <81170256+alexanderdong8@users.noreply.github.com>
2023-10-07 22:51:42 -0400

    pushing new frontend with functions instead of components

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,108 +1,86 @@
-import React, { Component } from 'react';
+import React, { useState, useCallback } from 'react';
 import "./App.css";
 
 interface FormProps {
   onFormSubmit: () => void;
 }
 
-class Form1 extends Component<FormProps> {
-  render() {
-    return (
-      <form className="form-container">
-        <div className="form-header">
-          Lettuce
-        </div>
-        <hr />
-        <div>
-          
-          <input type="file" id="field1" name="field1" /><br /><br />
-        </div>
-        <button type="submit" className="submit-button" onClick={this.props.onFormSubmit}>Submit</button>
-      </form>
-    );
-  }
-}
-
-class Form2 extends Component<FormProps> {
-  render() {
-    return (
-      <form className="form-container">
-        <div className="form-header">
-          Tomato
-        </div>
-        <hr />
-        <div>
-          
-          <input type="file" id="field2" name="field2" /><br /><br />
-        </div>
-        <button type="submit" className="submit-button" onClick={this.props.onFormSubmit}>Submit</button>
-      </form>
-    );
-  }
-}
-
-class Form3 extends Component<FormProps> {
-  render() {
-    return (
-      <form className="form-container">
-        <div className="form-header">
-          Basil
-        </div>
-        <hr />
-        <div>
-          
-          <input type="file" id="field3" name="field3" /><br /><br />
-        </div>
-        <button type="submit" className="submit-button" onClick={this.props.onFormSubmit}>Submit</button>
-      </form>
-    );
-  }
-}
-
-class NewForm extends Component {
-  render() {
-    return (
-      <div className="form-container">
-        <div className="form-header">
-          AI Model
-        </div>
-        <hr />
-        {/* Add the content of your new form here */}
+function Form1(props: FormProps) {
+  return (
+    <form className="form-container">
+      <div className="form-header">
+        Lettuce
       </div>
-    );
-  }
+      <hr />
+      <div>
+        <input type="file" id="field1" name="field1" /><br /><br />
+      </div>
+      <button type="submit" className="submit-button" onClick={props.onFormSubmit}>Submit</button>
+    </form>
+  );
 }
 
-interface AppState {
-  currentForm: React.ReactNode;
+function Form2(props: FormProps) {
+  return (
+    <form className="form-container">
+      <div className="form-header">
+        Tomato
+      </div>
+      <hr />
+      <div>
+        <input type="file" id="field2" name="field2" /><br /><br />
+      </div>
+      <button type="submit" className="submit-button" onClick={props.onFormSubmit}>Submit</button>
+    </form>
+  );
 }
 
-class App extends Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      currentForm: (
-        <div>
-          <Form1 onFormSubmit={this.handleFormSubmit} />
-          <Form2 onFormSubmit={this.handleFormSubmit} />
-          <Form3 onFormSubmit={this.handleFormSubmit} />
-        </div>
-      ),
-    };
-  }
+function Form3(props: FormProps) {
+  return (
+    <form className="form-container">
+      <div className="form-header">
+        Basil
+      </div>
+      <hr />
+      <div>
+        <input type="file" id="field3" name="field3" /><br /><br />
+      </div>
+      <button type="submit" className="submit-button" onClick={props.onFormSubmit}>Submit</button>
+    </form>
+  );
+}
 
-  handleFormSubmit = () => {
+function NewForm() {
+  return (
+    <div className="form-container">
+      <div className="form-header">
+        AI Model
+      </div>
+      <hr />
+      {/* Add the content of your new form here */}
+    </div>
+  );
+}
+
+function App() {
+  const handleFormSubmit = useCallback(() => {
     // Replace all three forms with the NewForm component
-    this.setState({ currentForm: <NewForm /> });
-  };
+    setCurrentForm(<NewForm />);
+  }, []);
 
-  render() {
-    return (
-      <div className="App">
-        {this.state.currentForm}
-      </div>
-    );
-  }
+  const [currentForm, setCurrentForm] = useState(
+    <div>
+      <Form1 onFormSubmit={handleFormSubmit} />
+      <Form2 onFormSubmit={handleFormSubmit} />
+      <Form3 onFormSubmit={handleFormSubmit} />
+    </div>
+  );
+
+  return (
+    <div className="App">
+      {currentForm}
+    </div>
+  );
 }
 
 export default App;
